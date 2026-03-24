f73e3b3ac010403ecb17404d303e4c69b885588c
Lav <lavdigikraftsocial@gmail.com>
2026-03-24 16:35:35 +0530

    commit

--- app/about/policy-on-familiarisation-programme/page.jsx
+++ app/about/policy-on-familiarisation-programme/page.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import Policy from './Policy'
 import Breadcrumbs from '@/components/Breadcrumbs'
+import breadcrumbs from "@/public/breadcrumbs/breadcrumbs.jpg";
 
 export default function page() {
+  const breadcrumbLinks = [
+    { label: "Home", url: "/" },
+    { label: "Disclosures", url: "/about/company" },
+  ];
   return (
     <div>
-        <Breadcrumbs/>
+       <Breadcrumbs
+        title="Investors"
+        links={breadcrumbLinks}
+        image={breadcrumbs}
+      />
       <Policy/>
     </div>
   )
